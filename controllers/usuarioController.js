daf564aee8f864c2f95c97a212d158816872dbb4
Samir <seperez043@gmail.com>
2023-09-06 23:01:27 -0500

    Control de errores en el formulario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,4 +1,4 @@
-import {check, validationResult} from 'express-validator';
+import { check, validationResult } from 'express-validator';
 import Usuario from '../models/Usuario.js';
 
 const formularioLogin = (req, res) => {
@@ -12,17 +12,37 @@ const formularioRegistro = (req, res) => {
 
   });
 }
-const registrar = async (req,res) =>{
+const registrar = async (req, res) => {
   //Validadicion
-  await check('nombre').notEmpty().withMessage('El nombre no puede ir vacio').run(req)
- 
-  let resultado = validationResult(req)
-  res.json(resultado.array());
+  await check('nombre').notEmpty().withMessage('Nombre no puede estar vacio').run(req)
+  await check('email').isEmail().withMessage('Eso no parece un email').run(req)
+  await check('password').isLength({ min: 8 }).withMessage('La contraseña debe contener por los menos 8 caracteres').run(req)
+  // await check('repetir_password').equals('password').withMessage('Las contraseñas no coiciden').run(req)
 
+  let resultado = validationResult(req)
+  console.log('resultado.array()', resultado.array());
+  //Verificar que el resultado este vacio 
+  if (!resultado.isEmpty()) {
+
+    return res.render('auth/registro', {
+      pagina: 'Crear cuenta',
+      errores: resultado.array(),
+      usuario: {
+        nombre: req.body.nombre,
+        email: req.body.email
+
+      }
+    })
+
+  }
+  const existeUsuario = await Usuario.findOne({ where: { email: req.body.email } })
+  console.log('existeUsuario', existeUsuario);
+  return
   const usuario = await Usuario.create(req.body)
   res.json(usuario);
 }
-const formulariooOlvidePassword = (req, res) => {
+
+const formularioOlvidePassword = (req, res) => {
   res.render('auth/olvide-password', {
     pagina: 'Recupera tu acceso Bienes Raices'
 
@@ -33,5 +53,5 @@ export {
   formularioLogin,
   formularioRegistro,
   registrar,
-  formulariooOlvidePassword
+  formularioOlvidePassword
 }
--- routes/usuariosRoutes.js
+++ routes/usuariosRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { formularioLogin, formularioRegistro, registrar, formulariooOlvidePassword } from '../controllers/usuarioController.js';
+import { formularioLogin, formularioRegistro, registrar, formularioOlvidePassword } from '../controllers/usuarioController.js';
 const router = express.Router();
 
 //Rountig
 router.get('/login', formularioLogin);
 router.get('/registro', formularioRegistro);
 router.post('/registro', registrar);
-router.get('/olvide-password', formulariooOlvidePassword);
+router.get('/olvide-password', formularioOlvidePassword);
 
 export default router;
